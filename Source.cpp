a79a653b392c421b0943dea57a658596eca04fce
Lydia Fritz <Lydia.Fritz@gcu.edu>
2020-05-09 09:18:12 -0700

    q class complete and tested

--- Source.cpp
+++ Source.cpp
@@ -3,21 +3,23 @@
 #include <iostream>
 #include <string>
 #include "myStack.h"
+#include "MyQ.h"
 
 using namespace std;
 
 int main() {
 
 	MyStack<int> stk;
+	MyQ<int> q;
 
 	for (size_t i = 0; i < 10; i++)
 	{
-		stk.push(i*2);
+		q.push(i*2);
 	}
 
-	while (!stk.empty()) {
-		cout << stk.top() << endl;
-		stk.pop();
+	while (!q.empty()) {
+		cout << q.front() << endl;
+		q.pop();
 	}
 
 	return 0;
